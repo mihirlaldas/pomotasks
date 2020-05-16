4eb186715c6c8a3ac88309bc4d3e8128888519d7
mihirlaldas <mihirlaldas@gmail.com>
2020-05-16 14:29:44 +0530

    converted timer to incremental watch

--- frontend/src/components/common/Timer.jsx
+++ frontend/src/components/common/Timer.jsx
@@ -4,38 +4,24 @@ export default class Timer extends React.Component {
     super(props);
     this.interval = null;
     this.state = {
-      minutes: 5,
+      hour: 0,
+      minutes: 0,
       second: 0,
-      showInput: false,
       isOn: false,
     };
   }
 
-  handleInput = (e) => {
-    clearInterval(this.interval);
-    this.setState({
-      minutes: e.target.value,
-    });
-  };
-  showInput = () => {
-    this.setState({
-      showInput: true,
-      minutes: null,
-      second: 0,
-    });
-  };
   start = () => {
     this.interval = setInterval(
       () =>
-        this.setState({
+        this.setState((prevState) => ({
           showInput: false,
           isOn: true,
-          second: this.state.second > 0 ? this.state.second - 1 : 59,
+          second: prevState.second < 60 ? prevState.second + 1 : 0,
           minutes:
-            this.state.second === 0
-              ? this.state.minutes - 1
-              : this.state.minutes,
-        }),
+            prevState.second === 60 ? prevState.minutes + 1 : prevState.minutes,
+          hour: prevState.minutes === 60 ? prevState.hour + 1 : prevState.hour,
+        })),
       1000
     );
   };
@@ -51,72 +37,37 @@ export default class Timer extends React.Component {
   reset = () => {
     this.stop();
     this.setState({
-      minutes: 5,
+      hour: 0,
+      minutes: 0,
       second: 0,
     });
   };
 
   render() {
-    if (this.state.minutes === 0 && this.state.second === 0) this.stop();
-    if (this.state.showInput) {
-      return (
-        <div>
-          <h1>Timer</h1>
-          <div className="form-group mx-auto w-50">
-            <input
-              className="form-control"
-              type="text"
-              value={this.state.minutes}
-              onChange={this.handleInput}
-              placeholder="Minutes"
-            />
-
-            <button onClick={this.start} className="btn btn-outline-primary">
-              Start
-            </button>
-            <button onClick={this.stop} className="btn btn-outline-primary">
-              Stop
-            </button>
-            <button onClick={this.reset} className="btn btn-outline-primary">
-              Reset
-            </button>
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div className="container bg-light shadow border rounded w-50 p-4">
-          <h1>Timer</h1>
-          <h1>
-            {this.state.minutes}
-            <small>m</small> {this.state.second}
-            <small>s</small>
-          </h1>
-          {/* toggle start and stop */}
-          {this.state.isOn ? (
-            <button onClick={this.stop} className="btn btn-danger px-3 m-1">
-              Stop
-            </button>
-          ) : (
-            <button onClick={this.start} className="btn btn-primary px-3 m-1">
-              Start
-            </button>
-          )}
-
-          <button
-            onClick={this.reset}
-            className="btn btn-outline-primary p-1 m-1"
-          >
-            Reset
+    return (
+      <div className="container bg-light shadow border rounded w-50 p-4">
+        <h1>Timer</h1>
+        <h1>
+          {this.state.hour} : {this.state.minutes} : {this.state.second}
+        </h1>
+        {/* toggle start and stop */}
+        {this.state.isOn ? (
+          <button onClick={this.stop} className="btn btn-danger px-3 m-1">
+            Stop
           </button>
-          <button
-            onClick={this.showInput}
-            className="btn btn-outline-primary p-1 m-1"
-          >
-            Set Minutes
+        ) : (
+          <button onClick={this.start} className="btn btn-primary px-3 m-1">
+            Start
           </button>
-        </div>
-      );
-    }
+        )}
+
+        <button
+          onClick={this.reset}
+          className="btn btn-outline-primary p-1 m-1"
+        >
+          Reset
+        </button>
+      </div>
+    );
   }
 }
